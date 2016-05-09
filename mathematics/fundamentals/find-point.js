2c6ec76b28c15b5391dc8b33b53bb0bf2afbb1d2
vagrant <vagrant@precise64.(none)>
2016-05-09 18:59:21 +0530

    Added file find-point.js

---
+++ mathematics/fundamentals/find-point.js
@@ -0,0 +1,29 @@
+/*
+A simple method to find symmetric point P about Q;
+
+Author: yogitaAP
+*/
+
+function processData(input) {
+    var inputsFormatted = input.split('\n');
+    var testCases = inputsFormatted.splice(0, 1);
+    
+    for(var i = 0; i < inputsFormatted.length; ++i) {
+        var iter = inputsFormatted[i];
+        var points = iter.split(' ');
+        var x = 2 * parseInt(points[2]) - parseInt(points[0]);
+        var y = 2 * parseInt(points[3]) - parseInt(points[1]);
+        console.log(x + ' ' + y);
+    }
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
